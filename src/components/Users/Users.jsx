8ae04554f31421d06490ccff448e260bce375d51
KubantsevAS <veng93@yandex.ru>
2022-11-23 23:53:26 +0300

    add pages with user data from API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,19 +4,43 @@ import axios from 'axios';
 
 class Users extends React.Component {
 
-    constructor(props) {
-
-        super(props)
-
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
             this.props.setUsers(response.data.items)
+            this.props.setTotalUsersCount(response.data.totalCount)
         })
-
     }
 
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+        this.props.setUsers(response.data.items)
+        })
+    }
+
+    
+
     render() {
+
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+        
+        let pages = [];
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i);
+        }
+        
+        
+
         return (
             <div>
+                <div>
+                    {pages.map(elem => {
+                        return <span 
+                        onClick={() => {this.onPageChanged(elem)}} 
+                        className={this.props.currentPage === elem && styles.selectedPage}>{elem}</span>
+                        })}
+                    
+                </div>
                 {
                     this.props.users.map(user => <div key={user.id}>
                         <span>
