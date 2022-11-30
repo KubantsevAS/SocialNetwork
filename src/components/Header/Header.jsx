d996136af723311b12b29ff98af13a0b0b4a1422
KubantsevAS <veng93@yandex.ru>
2022-11-30 20:13:53 +0300

    add login logic

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import pug from './images/PugLogoA.png';
 import classes from './Header.module.css';
+import { NavLink } from 'react-router-dom';
 
-const Header = () => {
+const Header = (props) => {
+    debugger
     return (
         <header className={classes.header}>
             <img src={pug} alt="Logo" width="275px" height="200px" />
+
+            <div className={classes.loginBlock}>
+                {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>}
+            </div>
         </header>
     );
 }
--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -3,12 +3,14 @@ import profileReducer from './profileReducer.js'
 import dialogsReducer from './dialogsReducer.js'
 import friendsPanelReducer from './friendsPanelReducer.js';
 import usersReducer from "./usersReducer.js";
+import authReducer from "./authReducer.js";
 
 let reducers = combineReducers({
     profilePage: profileReducer,   // profileReducer : profileReducer,
     messagesPage: dialogsReducer,
     friendsPanel: friendsPanelReducer,
-    usersPage: usersReducer
+    usersPage: usersReducer,
+    auth: authReducer
 })
 
 let store = createStore(reducers);
