9b678aa83aa5e795eb7407177f29809165c9ae02
KubantsevAS <veng93@yandex.ru>
2023-02-11 23:56:59 +0300

    text refactoring

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
                     <li className={styles.listItem}><span><b>Login:</b></span> <span>veng93@yandex.ru</span></li>
                     <li className={styles.listItem}><span><b>Password:</b></span> <span>12345</span></li>
                 </ul>
-                <p>Without logging in, you can only check the Users and About tabs.<br />
+                <p>Without logging in, you can only check the 'All users' and the 'About' tabs.<br />
                     After five failed login attempts, you will see a captcha that needs to be solved.</p>
             </div>
 
@@ -21,7 +21,7 @@ const About = () => {
                 <div className={styles.mainInfoItem} >On the 'Profile' tab, you can change your profile info, update status and photo. The API that was used does not allow you to save posts, so they are in the redux store.</div>
                 <div className={styles.mainInfoItem} >The 'Messages' tab is not working yet due to the same API capabilities. So you can only write messages in an improvised chat, without choosing a user.</div>
                 <div className={styles.mainInfoItem} >On the 'Friends' tab, you see a list of users you are followed to. You can go to each user's page by clicking on their name or photo.</div>
-                <div className={styles.mainInfoItem} >On the 'All users' tab, you can see all the Network users. You can also go to each user's page and also click the button to follow or unfollow user.</div>
+                <div className={styles.mainInfoItem} >On the 'All users' tab, you can see all the Network users. You can go to each user's page and also click the button to follow or unfollow user.</div>
                 <div className={styles.mainInfoItem} >
                     You can find my code in my public repository on GitHub by
                     <a href={"https://github.com/KubantsevAS/SocialNetwork"} target="_blank" rel="noreferrer">
